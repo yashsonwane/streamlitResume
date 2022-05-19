6b2d2e6e9a5ce741676f85051dceeef2cbf9d8df
yashsonwane <yash@discite.ai>
2022-05-20 00:01:07 +0530

    changes files

--- resumeParserStreamlit.py
+++ resumeParserStreamlit.py
@@ -2,8 +2,8 @@ import base64
 import streamlit as st
 import pandas as pd
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
-from ResumeEntitiesDrive import get_resume_entities
-from Gdrive import get_drive_folder_list
+# from ResumeEntitiesDrive import get_resume_entities
+# from Gdrive import get_drive_folder_list
 
 
 #page layout
@@ -56,11 +56,12 @@ def main():
             folder=drive_link[-1].strip()
             print(folder)
             try:
-                drive_list_df=get_drive_folder_list(folder)
+                pass
+                # drive_list_df=get_drive_folder_list(folder)
             except:
                 pass
-            print(drive_list_df)
-            flag=get_resume_entities(drive_list_df)
+            # print(drive_list_df)
+            # flag=get_resume_entities(drive_list_df)
             print("drive dateset")
                 
     #Selecting dataset
@@ -72,7 +73,7 @@ def main():
 
     st.subheader("Search Feature")
 
-    with st.form(key="data", clear_on_submit=True):
+    with st.form(key="data", clear_on_submit=False):
         dff = pd.DataFrame()
         first, second, third,fourth = st.columns([2,2,1,1])
         location_search = first.text_input('Location:').lower()
@@ -110,40 +111,62 @@ def main():
                     st.error("Please first create Google Drive dataset ")
 
             #read temp.csv
-            
-
             try:
+                scores = []
                 for i in range(len(df_entities)):
+                    score = 0
                     try:
                         for location_s in location_search.split(","):
                             if any([(location_s in i) for i in eval(df_entities.iloc[i].Location) if len(location_s) >=3]):
-                                results.append(df_entities.iloc[i])
+                                score += 1
+                            
                                 
                     except:
                         pass
                     try:
-                        for skills_s in skills_search.split(","):
-                            if any([(skills_s in i) for i in eval(df_entities.iloc[i].Skills) if len(skills_s) >=3]):
-                                results.append(df_entities.iloc[i])
+                        skills = skills_search.split(",")
+                        skills = [s.lstrip().rstrip() for s in skills]
+                        points_per_skill = 6/len(skills)
+                        for skill in skills:
+                            if skill in df_entities.iloc[i].Skills:
+                                score += points_per_skill
+
+
+
                     except:
                         pass
                     try:
                         
-                        if int(min_year_of_exp_search) <= df_entities.iloc[i].Year_of_exp <= int(max_year_of_exp_search):
-                            results.append(df_entities.iloc[i])
+                        if int(min_year_of_exp_search) < df_entities.iloc[i].Year_of_exp < int(max_year_of_exp_search)+1:
+                            score += 3
+                                
+                                #results .append(df_entities.iloc[i])
                     except:
                         pass
-                    
-                
+                    scores.append(score)
+            
+
+                df_entities['Scores'] = pd.Series(scores)
+
+                results = df_entities.sort_values(by=['Scores','Year_of_exp'], ascending=False).loc[:50]
+
+                col=['Email','Mobile','Year_of_exp','Location','Skills','Scores']
+            
+                results=results[col]   
                 # if results_skill and results_location:
 
-                dff=pd.DataFrame(results)
-                dff=dff.drop_duplicates(keep='first')
-            except:
-                pass
+                # dff=pd.DataFrame(results)
+                results=results.drop_duplicates(keep='first')
+                # print(results)    
+                # st.dataframe(results)
+                # AgGrid(dff)
                 
+            except:
+
+                pass
+            
             try:
-                gb = GridOptionsBuilder.from_dataframe(dff)
+                gb = GridOptionsBuilder.from_dataframe(results)
                 gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
                 gb.configure_side_bar() #Add a sidebar
                 gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
@@ -151,7 +174,7 @@ def main():
             
 
                 grid_response = AgGrid(
-                                        dff,
+                                        results,
                                         gridOptions=gridOptions,
                                         data_return_mode='AS_INPUT', 
                                         update_mode='MODEL_CHANGED', 
@@ -166,9 +189,10 @@ def main():
             
                 
                 selected = grid_response['selected_rows'] 
-                df = pd.DataFrame(selected) #
+                
             except:
                 pass
+
 
 
 
